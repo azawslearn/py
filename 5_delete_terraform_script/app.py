bcd10fc0fbd3bf9f1aa96516a54bc98711d662e1
azawslearn <134204024+azawslearn@users.noreply.github.com>
2023-10-07 22:41:26 +0300

    Update app.py

--- 5_delete_terraform_script/app.py
+++ 5_delete_terraform_script/app.py
@@ -1,3 +1,5 @@
+### WINDOWS SCRIPT ###
+
 import glob
 import os
 import shutil
@@ -19,5 +21,34 @@ for item in glob.iglob(pattern, recursive=True):
 pattern = r"C:\Users\Ivan\Desktop\To_delete\**\.terraform.*"
 for item in glob.iglob(pattern, recursive=True):
     # delete file
+    print("Deleting:", item)
+    os.remove(item)
+
+
+### LINUX SCRIPT ###
+
+import glob
+import os
+import shutil
+
+# Search pattern inside folders and subfolders recursively
+# Search all .terraform folders
+pattern = "/home/ivan/To_delete/**/.terraform"
+for item in glob.glob(pattern, recursive=True):
+    # Delete folder
+    print("Deleting:", item)
+    shutil.rmtree(item)
+
+# Search all terraform* files
+pattern = "/home/ivan/To_delete/**/terraform*"
+for item in glob.glob(pattern, recursive=True):
+    # Delete file
+    print("Deleting:", item)
+    os.remove(item)
+
+# Search all .terraform.* files
+pattern = "/home/ivan/To_delete/**/.terraform.*"
+for item in glob.glob(pattern, recursive=True):
+    # Delete file
     print("Deleting:", item)
     os.remove(item)
